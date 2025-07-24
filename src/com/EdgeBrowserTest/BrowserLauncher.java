a7c3433407485a15400b984ccbecfee3b8c65b9a
saiharshith312004 <parlapalli2004@gmail.com>
2025-07-24 17:16:03 +0530

    third commit

--- src/com/EdgeBrowserTest/BrowserLauncher.java
+++ src/com/EdgeBrowserTest/BrowserLauncher.java
@@ -12,31 +12,31 @@ public class BrowserLauncher {
         driver.get("https://opensource-demo.orangehrmlive.com/");
         System.out.println("Current URL: " + driver.getCurrentUrl());
         System.out.println("Title: " + driver.getTitle());
-        System.out.println("Page Source Length: " + driver.getPageSource().length());
+        System.out.println("Page Source Length: " + driver.getPageSource());
         System.out.println("--------------------------------------------------");
         // 2. OpenCart
         driver.get("https://demo.opencart.com/");
         System.out.println("Current URL: " + driver.getCurrentUrl());
         System.out.println("Title: " + driver.getTitle());
-        System.out.println("Page Source Length: " + driver.getPageSource().length());
+        System.out.println("Page Source Length: " + driver.getPageSource());
         System.out.println("--------------------------------------------------");
         // 3. Bing
         driver.get("http://www.bing.com");
         System.out.println("Current URL: " + driver.getCurrentUrl());
         System.out.println("Title: " + driver.getTitle());
-        System.out.println("Page Source Length: " + driver.getPageSource().length());
+        System.out.println("Page Source Length: " + driver.getPageSource());
         System.out.println("--------------------------------------------------");
         // 4. FreeCRM
         driver.get("https://www.freecrm.com/");
         System.out.println("Current URL: " + driver.getCurrentUrl());
         System.out.println("Title: " + driver.getTitle());
-        System.out.println("Page Source Length: " + driver.getPageSource().length());
+        System.out.println("Page Source Length: " + driver.getPageSource());
         System.out.println("--------------------------------------------------");
         // 5. eBay (Half)
         driver.get("http://www.half.ebay.com");
         System.out.println("Current URL: " + driver.getCurrentUrl());
         System.out.println("Title: " + driver.getTitle());
-        System.out.println("Page Source Length: " + driver.getPageSource().length());
+        System.out.println("Page Source Length: " + driver.getPageSource());
         System.out.println("--------------------------------------------------");
         driver.quit();
     }
